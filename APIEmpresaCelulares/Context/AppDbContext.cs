6d34dc979ef5f28fd9f8a854806dfe4043a0845e
VictorLima <victoraugustolima1@gmail.com>
2022-12-13 17:35:39 -0300

    banco de dados registrado

---
+++ APIEmpresaCelulares/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using APIEmpresaCelulares.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIEmpresaCelulares.Context
+{
+    public class AppDbContext : DbContext
+        
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Mensagem> Mensagens { get; set; }
+    }
+}
