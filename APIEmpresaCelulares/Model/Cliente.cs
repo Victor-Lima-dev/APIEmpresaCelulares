fb88f8249d58fe5409c4a85165d35c127cfd219a
VictorLima <victoraugustolima1@gmail.com>
2022-12-13 17:30:01 -0300

    Adicionar arquivos de projeto.

---
+++ APIEmpresaCelulares/Model/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace APIEmpresaCelulares.Model
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Genero { get; set; }
+        public string Regiao { get; set; }
+        
+
+    }
+}
---
+++ APIEmpresaCelulares/Model/Mensagem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace APIEmpresaCelulares.Model
+{
+    public class Mensagem
+    {
+        public int MensagemId { get; set; }
+        public string Texto { get; set; }
+        public DateTime Data { get; set; }
+
+        public List<int> ClienteId { get; set; }
+    }
+}
